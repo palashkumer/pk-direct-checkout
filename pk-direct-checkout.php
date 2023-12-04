ef48898d1bd13e596b6ea761f9dd0e57f2a4ba28
Palash Kumer <palashkumer237@gmail.com>
2023-12-05 03:02:55 +0600

    initial commit

---
+++ pk-direct-checkout.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: PK WooCom Direct Checkout
+ * Description: This is about wooCom direct checkout button
+ * Version: 1.0.0
+ * Author: Palash Kumer
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
