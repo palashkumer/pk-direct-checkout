5235d9c696a0516ab44c9ebbc47aceea04e06f15
Palash Kumer <palashkumer237@gmail.com>
2023-12-05 13:11:46 +0600

    feat:  Settings page & buy now button labeling

--- pk-direct-checkout.php
+++ pk-direct-checkout.php
@@ -43,3 +43,69 @@ function pkdc_product_page() {
 	include plugin_dir_path( __FILE__ ) . 'templates/pkdc-button.php';
 }
 add_action( 'woocommerce_after_add_to_cart_button', 'pkdc_product_page' );
+
+
+
+/**
+ * Function to add menu page
+ *
+ * @return void
+ */
+function pk_direct_checkout_menu_page() {
+	add_menu_page(
+		'PK Direct Checkout',
+		'PK Direct Checkout',
+		'manage_options',
+		'pk-direct-checkout',
+		'pk_direct_checkout_menu_page_content',
+		'',
+		5
+	);
+
+	// Add submenu page.
+	add_submenu_page(
+		'pk-direct-checkout',
+		'Settings',
+		'Settings',
+		'manage_options',
+		'pk-direct-checkout-settings',
+		'pk_direct_checkout_settings_page_content'
+	);
+}
+add_action( 'admin_menu', 'pk_direct_checkout_menu_page' );
+
+
+/**
+ *  Callback function for the main menu page
+ *
+ * @return void
+ */
+function pk_direct_checkout_menu_page_content() {
+	echo '<div class="wrap"><h2>PK Direct Checkout</h2><p>Hello,I am Palash</p></div>';
+}
+
+/**
+ * Callback function for the submenu page
+ *
+ * @return void
+ */
+function pk_direct_checkout_settings_page_content() {
+	?>
+	<div class="wrap">
+		<h2>PK Direct Checkout Settings</h2>
+
+		<form method="post" action="">
+			<label for="buy_now_button_label">Buy Now Button Label: </label>
+			<input type="text" name="buy_now_button_label" id="buy_now_button_label" value="<?php echo esc_attr( get_option( 'buy_now_button_label', 'Buy Now' ) ); ?>" />			
+			<input type="submit" class="button button-primary" value="Save Changes" />
+		</form>
+		<?php
+		if ( isset( $_POST['buy_now_button_label'] ) ) {
+			$new_label = sanitize_text_field( wp_unslash( $_POST['buy_now_button_label'] ) );
+			update_option( 'buy_now_button_label', $new_label );
+			echo '<div class="updated"><p>Settings saved!</p></div>';
+		}
+		?>
+	</div>
+	<?php
+}
--- templates/pkdc-button.php
+++ templates/pkdc-button.php
@@ -1,10 +1,11 @@
 <?php
 global $product;
-$product_id   = $product->get_id();
-$checkout_url = wc_get_checkout_url();
-$buy_now_url  = esc_url( add_query_arg( 'add-to-cart', $product_id, $checkout_url ) );
+$product_id    = $product->get_id();
+$checkout_url  = wc_get_checkout_url();
+$buy_now_url   = esc_url( add_query_arg( 'add-to-cart', $product_id, $checkout_url ) );
+$buy_now_label = get_option( 'buy_now_button_label', 'Buy Now' );
 ?>
 
 <a href="<?php echo $buy_now_url; ?>" class="buy-now-button">
-	<?php esc_html_e( 'Buy Now', 'your-text-domain' ); ?>
+	<?php echo esc_html( $buy_now_label ); ?>
 </a>
