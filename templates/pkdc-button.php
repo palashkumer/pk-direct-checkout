89598e504f1b603b0e3eb7b5a398fea682406bdb
Palash Kumer <palashkumer237@gmail.com>
2023-12-06 12:54:50 +0600

    feat: add more input fields

--- templates/pkdc-button.php
+++ templates/pkdc-button.php
@@ -1,11 +1,20 @@
 <?php
 global $product;
-$product_id    = $product->get_id();
-$checkout_url  = wc_get_checkout_url();
-$buy_now_url   = esc_url( add_query_arg( 'add-to-cart', $product_id, $checkout_url ) );
-$buy_now_label = get_option( 'buy_now_button_label', 'Buy Now' );
-?>
+if ( $product->is_type( 'simple' ) ) {
+			$product_id = $product->get_id();
 
-<a href="<?php echo $buy_now_url; ?>" class="buy-now-button">
-	<?php echo esc_html( $buy_now_label ); ?>
-</a>
+				$checkout_url  = wc_get_checkout_url();
+				$buy_now_url   = esc_url( add_query_arg( 'add-to-cart', $product_id, $checkout_url ) );
+				$buy_now_label = get_option( 'buy_now_button_label', 'Buy Now' );
+				$button_color  = get_option( 'buy_now_button_color', '#0073e5' );
+				$font_color    = get_option( 'buy_now_font_color', '#ffffff' );
+				$font_size     = get_option( 'buy_now_font_size', '16px' );
+
+	?>
+
+			<a href="<?php echo esc_url( $buy_now_url ); ?>" class="buy-now-button" style="background-color: <?php echo esc_attr( $button_color ); ?>;
+				color: <?php echo esc_attr( $font_color ); ?>;
+				font-size: <?php echo esc_attr( $font_size ); ?>;">
+				<?php echo esc_html( $buy_now_label ); ?>
+			</a>
+<?php } ?>
